d5de4edcd147b054e97be99b78a3abe76266af4c
erin dougher <ejdougher@gmail.com>
2020-11-30 07:04:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 
-
+=begin
 def reverse_each_word(sentence)
 words = sentence.split(" ")
 new_sentence = []
@@ -10,7 +10,7 @@ new_sentence.join(" ")
 end
 p new_sentence
 end
-
+=end
 def uses_collect(sentence_2)
   words = sentence.split(" ")
   new_sentence = []
