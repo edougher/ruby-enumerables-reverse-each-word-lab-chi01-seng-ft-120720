5e163e25cf3753d5b90e749af8f2918182f31106
erin dougher <ejdougher@gmail.com>
2020-11-30 07:56:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,12 +6,7 @@ words.each do |words|
 words = words.reverse
 new_sentence << words
 end
-words.collect do |str|
- words = str.reverse
- new_sentence << words
-end
-
- p new_sentence.join(" ")
+p new_sentence.join(" ")
 end
 sentence1 = "Hello there, and how are you?"
 reverse_each_word(sentence1)
